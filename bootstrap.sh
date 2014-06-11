9395f6991166aac055e24cd10b58847b305a36ce
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2014-06-11 20:38:06 +0200

    Install Puppet 3.6.1 by default.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,12 +7,12 @@ apt-get upgrade -y
 apt-get install git-core sudo lsb-release -y
 
 CODENAME=$(lsb_release -sc)
-PUPPET_VERSION='2.7.24'
+PUPPET_VERSION='3.6.1-1puppetlabs1'
 
 cd /tmp
 wget -q http://apt.puppetlabs.com/puppetlabs-release-$CODENAME.deb
 dpkg -i puppetlabs-release-$CODENAME.deb
 rm -f puppetlabs-release-$CODENAME.deb
 apt-get update
-apt-get install puppet-common=$PUPPET_VERSION-1puppetlabs1 -y
-apt-get install puppet=$PUPPET_VERSION-1puppetlabs1 -y
+apt-get install puppet-common=$PUPPET_VERSION puppet=$PUPPET_VERSION \
+                libaugeas-ruby -y
