227f124200269b77c7606b16585f2d84bbea0a9b
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2014-11-06 15:59:46 +0100

    Code lint

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,13 +11,13 @@ PUPPET_VERSION='3.6.2-1puppetlabs1'
 FACTER_VERSION='2.1.0-1puppetlabs1'
 
 cd /tmp
-wget -q http://apt.puppetlabs.com/puppetlabs-release-$CODENAME.deb
-dpkg -i puppetlabs-release-$CODENAME.deb
-rm -f puppetlabs-release-$CODENAME.deb
+wget -q "http://apt.puppetlabs.com/puppetlabs-release-${CODENAME}.deb"
+dpkg -i "puppetlabs-release-${CODENAME}.deb"
+
 apt-get update
 apt-get install puppet-common=$PUPPET_VERSION puppet=$PUPPET_VERSION \
                 facter=$FACTER_VERSION libaugeas-ruby -y
 
 wget https://github.com/bdossantos.keys
-mkdir -m 0700 ~/.ssh
+mkdir -p -m 0700 ~/.ssh
 cat /tmp/bdossantos.keys > ~/.ssh/authorized_keys
