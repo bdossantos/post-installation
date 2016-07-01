4b69e2413eed00144c9ffed15372434dcd8d62f8
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2016-07-01 10:58:15 +0200

    refactor: don't pin puppet version, ensure sudo is present

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,16 +11,13 @@ apt-get upgrade -y
 apt-get install git-core sudo lsb-release wget -y
 
 CODENAME=$(lsb_release -sc)
-PUPPET_VERSION='3.7.5-1puppetlabs1'
-FACTER_VERSION='2.4.3-1puppetlabs1'
 
 cd /tmp
 wget -q "http://apt.puppetlabs.com/puppetlabs-release-${CODENAME}.deb"
 dpkg -i "puppetlabs-release-${CODENAME}.deb"
 
 apt-get update
-apt-get install puppet-common=$PUPPET_VERSION puppet=$PUPPET_VERSION \
-                facter=$FACTER_VERSION libaugeas-ruby -y
+apt-get install puppet-common puppet facter libaugeas-ruby sudo -y
 
 wget https://github.com/bdossantos.keys
 mkdir -p -m 0700 ~/.ssh
