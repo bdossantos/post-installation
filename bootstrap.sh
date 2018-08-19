74a1f0fb0ede0e37530116f62e26cef6ecbac7f2
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2018-08-19 13:44:46 +0200

    chore: don't install puppet anymore

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,12 +13,9 @@ apt-get install git-core sudo lsb-release wget -y
 CODENAME=$(lsb_release -sc)
 
 cd /tmp
-wget -q "http://apt.puppetlabs.com/puppet-release-${CODENAME}.deb"
-dpkg -i "puppet-release-${CODENAME}.deb"
-
-apt-get update
-apt-get install puppet-common puppet facter ruby-augeas sudo -y
-
 wget https://github.com/bdossantos.keys
 mkdir -p -m 0700 ~/.ssh
 cat /tmp/bdossantos.keys > ~/.ssh/authorized_keys
+
+apt-get update
+apt-get install facter sudo -y
